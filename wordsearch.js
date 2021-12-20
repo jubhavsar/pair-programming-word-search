536dd5c3a6b20d1941cb833909597ae87e6eae9d
Juhi Bhavsar <63750897+jubhavsar@users.noreply.github.com>
2021-12-19 23:40:13 -0500

    Update wordsearch.js

--- wordsearch.js
+++ wordsearch.js
@@ -5,6 +5,17 @@ const wordSearch = (letters, word) => {
             return true;
         }
     }
+    for (let j = 0; j < letters[0].length; j++) {
+      let str = "";
+  
+      for (let i = 0; i < letters.length; i++) {
+        str += letters[i][j];
+      }
+  
+      if (str.includes(word)) {
+        return true;
+      }
+    }
     return false;
 }
 
